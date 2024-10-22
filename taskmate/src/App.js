c47322dbf6f48b3a9ecdc0e3db2b143df722f7e1
Trevor Taylor <sirtrevis42@gmail.com>
2024-10-22 14:29:29 -0500

    setting up counter for state practice

--- taskmate/src/App.js
+++ taskmate/src/App.js
@@ -1,7 +1,20 @@
 import './App.css';
 
 const App = () => {
-    return <div className="App"></div>;
+  let counter = 0;
+  return (
+    <div className="App">
+      <div className="box">
+        <p>{counter}</p>
+        <button className="add" onClick={() => counter++}>
+          ADD
+        </button>
+        <button className="sub" onClick={() => counter++}>
+          SUB
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
