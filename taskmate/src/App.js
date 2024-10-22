89788c65ceeeed8098b76ad3e7f02f68de5e936a
Trevor Taylor <sirtrevis42@gmail.com>
2024-10-22 15:42:06 -0500

    testing arrow function state update

--- taskmate/src/App.js
+++ taskmate/src/App.js
@@ -5,24 +5,33 @@ const App = () => {
   const [count, setCount] = useState(0);
 
   const handleAdd = () => {
-    setCount(count + 1);
-    console.log(`Counter: ${count}`);
+    setCount(count => count + 1);
+    setCount(count => count + 1);
+    setCount(count => count + 1);
+    console.log(`Count: ${count}`);
   };
 
   const handleSubtract = () => {
     setCount(count - 1);
-    console.log(`Counter: ${count}`);
+    console.log(`Count: ${count}`);
+  };
+
+  const handleReset = () => {
+    setCount(0);
   };
 
   return (
     <div className="App">
       <div className="box">
         <p>{count}</p>
-        <button className="add" onClick={handleAdd}>
+        <button onClick={handleAdd} className="add">
           ADD
         </button>
-        <button className="sub" onClick={handleSubtract}>
+        <button onClick={handleSubtract} className="sub">
           SUB
+        </button>
+        <button onClick={handleReset} className="reset">
+          RESET
         </button>
       </div>
     </div>
