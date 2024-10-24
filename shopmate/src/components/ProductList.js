06250bb9c077da1a48041ae2ab35c1e733852b1f
Trevor Taylor <sirtrevis42@gmail.com>
2024-10-24 15:17:07 -0500

    error handling in fetch

--- shopmate/src/components/ProductList.js
+++ shopmate/src/components/ProductList.js
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 import { useFetch } from '../hooks/useFetch';
+// import Loading from '../assets/loading.gif';
 
 export const ProductList = () => {
   const [url, setUrl] = useState('http://localhost:8000/products');
-  const { data: products } = useFetch(url);
+  const { data: products, loading, error } = useFetch(url);
 
   return (
     <section>
@@ -11,6 +12,8 @@ export const ProductList = () => {
         <button onClick={() => setUrl('http://localhost:8000/products')}>All</button>
         <button onClick={() => setUrl('http://localhost:8000/products?instock=true')}>In Stock Only</button>
       </div>
+      {loading && <p className="loading">Loading...</p>}
+      {error && <p className="error">{error}</p>}
 
       {products &&
         products.map(product => (
--- shopmate/src/hooks/useFetch.js
+++ shopmate/src/hooks/useFetch.js
@@ -2,15 +2,28 @@ import { useState, useEffect } from 'react';
 
 export const useFetch = url => {
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch(url);
-      const result = await response.json();
-      setData(result);
+      setLoading(true);
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const result = await response.json();
+        setLoading(false);
+        setData(result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setLoading(false);
+        setError(error.message);
+      }
     };
     fetchData();
   }, [url]);
 
-  return { data };
+  return { data, loading, error };
 };
