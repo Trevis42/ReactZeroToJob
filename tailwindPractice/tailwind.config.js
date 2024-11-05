e220de60ae2eacd14cddf5a3046b7e4536aad77c
Trevor Taylor <sirtrevis42@gmail.com>
2024-11-05 16:41:33 -0600

    config and custom color range

--- tailwindPractice/tailwind.config.js
+++ tailwindPractice/tailwind.config.js
@@ -3,7 +3,18 @@ tailwind.config = {
   theme: {
     extend: {
       colors: {
-        clifford: '#da373d',
+        purple: '#540081',
+        rawr: {
+          100: '#d5fefe',
+          200: '#abfdfd',
+          300: '#82fdfd',
+          400: '#58fcfc',
+          500: '#2efbfb',
+          600: '#25c9c9',
+          700: '#1c9797',
+          800: '#126464',
+          900: '#093232',
+        },
       },
     },
   },
