d4536e8f50c00886ae36f4a4025cc4163164e56c
Trevor Taylor <sirtrevis42@gmail.com>
2024-11-06 14:47:36 -0600

    soundmate demo

---
+++ soundmateTutorial/tailwind.config.js
@@ -0,0 +1,25 @@
+tailwind.config = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        purple: '#8200aa',
+        rawr: {
+          100: '#d5fefe',
+          200: '#abfdfd',
+          300: '#82fdfd',
+          400: '#58fcfc',
+          500: '#2efbfb',
+          600: '#25c9c9',
+          700: '#1c9797',
+          800: '#126464',
+          900: '#093232',
+        },
+        logo: {
+          700: '#1C59AE',
+          800: '#114a9a',
+        },
+      },
+    },
+  },
+};
